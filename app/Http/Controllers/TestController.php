11fdc666b685de1fa30fc7ce6f16a7547c55cd64
Gerard <gerardtrejo12@gmail.com>
2019-05-30 14:25:03 -0500

    App advances

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class TestController extends Controller
 {
+
     public function welcome()
     {
-    	
-    return  view ('welcome');
-}
+    	$categories = Category::has('products')->get();
+    	return view('welcome')->with(compact('categories'));
+    }
+
 }
